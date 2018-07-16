47d175b32310b7175aacb04db91e725e193a8b0f
huchun <huun82972277@163.com>
2018-07-16 23:23:52 +0800

    添加jar，基类

---
+++ app/src/main/java/com/client/huaccount/HuApplication.java
@@ -0,0 +1,16 @@
+package com.client.huaccount;
+
+import android.app.Application;
+
+/**
+ * Created by l on 2018/7/15.
+ */
+
+public class HuApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/client/huaccount/MainActivity.java
+++ app/src/main/java/com/client/huaccount/MainActivity.java
@@ -8,12 +8,13 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import com.client.huaccount.base.BaseActivity;
+
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
